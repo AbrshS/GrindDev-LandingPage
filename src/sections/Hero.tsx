c8f7c948c0711c6c783becb3b93577182b4d9f2e
AbrshS <abrshs114@gmail.com>
2025-04-23 22:48:36 +0300

    nav and hero section completed

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,26 @@
+import Button from "@/components/Button";
+import button from "@/components/Button";
+export default function Hero() {
+    return (
+        <section className="py-24">
+            <div className="container">
+                <div className="flex justify-center"></div>
+                <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 text-neutral-950  rounded-full font-semibold ">
+                    ✨ $7.5M seed round raised
+                </div>
+                <h1 className="text-6xl font-medium text-center mt-6">
+                    Impactful design, created effortlessly
+                </h1>
+                <p className="text-center text-xl text-white/50 mt-8">
+                    Design tools shoudn&apos;t slow you down. Layes combines
+                    powerful features with an intuitive interface that keeps you
+                    in your creative flow.
+                </p>
+                <form className="flex border border-white/15 rounded-full p-2 mt-8 ">
+                    <input type="email" placeholder="Enter your email" className="bg-transparent px-4" />
+                    <Button type="submit" variant="primary" className="whitespace-nowrap" size="sm">Sign Up</Button>
+                </form>
+            </div>
+        </section>
+    );
+}
